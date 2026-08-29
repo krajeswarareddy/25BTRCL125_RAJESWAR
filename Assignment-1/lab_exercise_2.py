f62ccf301ce9a58de4e82415777f38afc974b672
krajeswarareddy <reddykrajeswar18@gmail.com>
2026-08-30 00:14:23 +0530

    Add Lab Exercise 2

---
+++ Assignment-1/lab_exercise_2.py
@@ -0,0 +1,11 @@
+# Lab Exercise 2: Variables, Operators, and Input/Output
+
+name = input("Enter your name: ")
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("\nName:", name)
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
